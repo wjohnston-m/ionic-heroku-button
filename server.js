9f006ab435f2c513489b0c7d5ae3433c778be2b2
Will Johnston <wjohnston@midlandira.com>
2020-05-21 21:49:42 -0500

    Trying to get login working

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 var express = require('express'),
-    app = express();
+    app = express(),
+    jsforce = require('jsforce');
 
 app.use(express.static('www'));
 
@@ -18,3 +19,19 @@ app.set('port', process.env.PORT || 5000);
 app.listen(app.get('port'), function () {
     console.log('Express server listening on port ' + app.get('port'));
 });
+
+app.use(login);
+
+function login() {
+    console.log('logging in');
+    var conn = new jsforce.Connection({
+      loginUrl : 'https://test.salesforce.com'
+    });
+    conn.login('wjohnston@midlandira.com', 'Fr0zen34ZSmGGKJEMDRtd2Pz7cSJyAqf', function(err, res) {
+      if (err) { return console.error(err); }
+      conn.query('SELECT Id, Name FROM Account', function(err, res) {
+        if (err) { return console.error(err); }
+        console.log(res);
+      });
+    });
+}
--- www/js/controllers.js
+++ www/js/controllers.js
@@ -2,18 +2,7 @@ angular.module('starter.controllers', [])
 
 .controller('DashCtrl', function($scope) {
   $scope.login = function(){
-    console.log('logging in');
-    var jsforce = require('jsforce');
-    var conn = new jsforce.Connection({
-      loginUrl : 'https://test.salesforce.com'
-    });
-    conn.login('wjohnston@midlandira.com', 'Fr0zen34ZSmGGKJEMDRtd2Pz7cSJyAqf', function(err, res) {
-      if (err) { return console.error(err); }
-      conn.query('SELECT Id, Name FROM Account', function(err, res) {
-        if (err) { return console.error(err); }
-        console.log(res);
-      });
-    });
+    
   }
 })
 
